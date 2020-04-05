cb74ce19bd68e26db563bf009b0b1dba7b419f27
andytyler <atyler1@sheffield.ac.uk>
2020-04-05 14:28:47 +0100

    added post request to add url to ddb

--- index.js
+++ index.js
@@ -4,23 +4,17 @@ var imageNumber = 0
 var startDelay = 5
 var rateOfDecay = 2
 
+// var XMLHttpRequest = require('XMLHttpRequest')
+
 var images = []
-const img = document.getElementById('catchphraseImage')
+const url = document.getElementById('entry-text-box')
+const urltext = url.innerText
 
-async function setUpPage () {
-  const response = await fetch('https://ibnibvvcs7.execute-api.eu-west-1.amazonaws.com/Prod/list/', {})
-  const json = await response.json()
-  images = await json.catchphrases
+function XHRPost () {
+  var xhttp = new XMLHttpRequest()
+  var apiUrl = 'https://ibnibvvcs7.execute-api.eu-west-1.amazonaws.com/Prod/add'
+
+  xhttp.open('POST', apiUrl, true)
+  xhttp.setRequestHeader('Content-type', 'application/json')
+  xhttp.send('urltext')
 }
-
-function changeImage () {
-  img.setAttribute('src', images[imageNumber])
-  imageNumber++
-  if (imageNumber >= images.length) {
-    imageNumber = 0
-  }
-  console.log('changeImage() called')
-}
-
-setUpPage()
-setInterval(changeImage, 10000)
