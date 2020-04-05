749a1c3fa29b5a5b7248d7a9fa269628a5b51291
andytyler <atyler1@sheffield.ac.uk>
2020-04-05 13:57:35 +0100

    added new page

---
+++ home.js
@@ -0,0 +1,26 @@
+var coverArea = document.getElementById('coverArea')
+var totalCoverBoxes = 100
+var imageNumber = 0
+var startDelay = 5
+var rateOfDecay = 2
+
+var images = []
+const img = document.getElementById('catchphraseImage')
+
+async function setUpPage () {
+  const response = await fetch('https://ibnibvvcs7.execute-api.eu-west-1.amazonaws.com/Prod/list/', {})
+  const json = await response.json()
+  images = await json.catchphrases
+}
+
+function changeImage () {
+  img.setAttribute('src', images[imageNumber])
+  imageNumber++
+  if (imageNumber >= images.length) {
+    imageNumber = 0
+  }
+  console.log('changeImage() called')
+}
+
+setUpPage()
+setInterval(changeImage, 10000)
