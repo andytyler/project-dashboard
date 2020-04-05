28fb8a4b599afe4ce01bcce47dd022d41ecb0c5a
andytyler <atyler1@sheffield.ac.uk>
2020-04-05 11:14:16 +0100

    updated fetch URL endpoint to the new dynamic endpoint

--- public/app.js
+++ public/app.js
@@ -5,24 +5,22 @@ var startDelay = 5
 var rateOfDecay = 2
 
 var images = []
-let img = document.getElementById('catchphraseImage')
+const img = document.getElementById('catchphraseImage')
 
-
-async function setUpPage() {
-	const response = await fetch('https://ibnibvvcs7.execute-api.eu-west-1.amazonaws.com/Prod/hello/', {})
-	const json = await response.json()
-	images = await json.catchphrases
+async function setUpPage () {
+  const response = await fetch('https://ibnibvvcs7.execute-api.eu-west-1.amazonaws.com/Prod/list/', {})
+  const json = await response.json()
+  images = await json.catchphrases
 }
 
-function changeImage() {
-	img.setAttribute('src', images[imageNumber])
-	imageNumber++
-	if (imageNumber >= images.length) {
-		imageNumber = 0
-	}
-	console.log('changeImage() called')
+function changeImage () {
+  img.setAttribute('src', images[imageNumber])
+  imageNumber++
+  if (imageNumber >= images.length) {
+    imageNumber = 0
+  }
+  console.log('changeImage() called')
 }
-
 
 setUpPage()
 setInterval(changeImage, 10000)
