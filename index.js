eaee0f980f9c7b9f1b7e244ff63ff958811604b9
andytyler <atyler1@sheffield.ac.uk>
2020-04-05 18:57:24 +0100

    added getted list

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const url = document.getElementById('entry-text-box')
 const urltext = url.innerText
 var data = JSON.stringify({ url: urltext })
 
+async function getList () {
+  const response = await fetch('https://ibnibvvcs7.execute-api.eu-west-1.amazonaws.com/Prod/list/', {})
+  const json = await response.json()
+  images = await json.catchphrases
+  const url = document.getElementById('exit-text-box')
+  url.innerText = images
+}
+
 function XHRPost () {
   var xhttp = new XMLHttpRequest()
   var apiUrl = 'https://ibnibvvcs7.execute-api.eu-west-1.amazonaws.com/Prod/add'
