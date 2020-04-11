2ca02d616ffe818fd1769123e1ccd8a8206da83f
andytyler <atyler1@sheffield.ac.uk>
2020-04-11 21:16:27 +0100

    implemented front end delete

--- index.js
+++ index.js
@@ -36,3 +36,22 @@ async function XHRPost () {
   }
   http.send(data)
 }
+
+async function XHRDelete () {
+  var http = new XMLHttpRequest()
+  var url = 'https://mhs6nqd42a.execute-api.eu-west-1.amazonaws.com/Prod/delete/'
+
+  const box = document.getElementById('entry-text-box')
+  var data = JSON.stringify({ url: box.value })
+  http.open('POST', url, true)
+
+  // Send the proper header information along with the request
+  http.setRequestHeader('Content-type', 'application/json')
+
+  http.onreadystatechange = function () { // Call a function when the state changes.
+    if (http.readyState == 4 && http.status == 200) {
+      console.log(http.responseText)
+    }
+  }
+  http.send(data)
+}
