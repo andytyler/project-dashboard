915f80f787ba686bd7dfd6a0a4bc2eb0e579c91f
andytyler <atyler1@sheffield.ac.uk>
2020-04-11 20:37:11 +0100

    some updates

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-var coverArea = document.getElementById('coverArea')
-var totalCoverBoxes = 100
-var imageNumber = 0
-var startDelay = 5
-var rateOfDecay = 2
-
-// var XMLHttpRequest = require('XMLHttpRequest')
-
 var images = []
+const display = document.getElementById('display')
+const exitTextBox = document.getElementById('exit-text-box')
 const url = document.getElementById('entry-text-box')
 const urltext = url.value
 var data = JSON.stringify({ url: urltext })
@@ -15,19 +9,17 @@ async function getList () {
   const response = await fetch('https://mhs6nqd42a.execute-api.eu-west-1.amazonaws.com/Prod/list/', {})
   const json = await response.json()
   images = await json.catchphrases
-  const url = document.getElementById('exit-text-box')
-  url.innerText = JSON.stringify(images)
+
+  display.innerHTML = ''
+
+  images.forEach((item) => {
+    var btn = document.createElement('h6') // Create a <button> element
+    btn.innerHTML = item // Insert text
+    display.appendChild(btn)
+  })
 }
 
 async function XHRPost () {
-  // const url = document.getElementById('entry-text-box')
-  // var data = JSON.stringify({ url: url.value })
-
-  // await fetch('https://mhs6nqd42a.execute-api.eu-west-1.amazonaws.com/Prod/add/', {
-  //   method: 'POST', // or 'PUT'
-  //   body: JSON.stringify(data)
-  // })
-
   var http = new XMLHttpRequest()
   var url = 'https://mhs6nqd42a.execute-api.eu-west-1.amazonaws.com/Prod/add/'
   const box = document.getElementById('entry-text-box')
