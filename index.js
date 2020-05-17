d4e93d52f63425d876d77a0ca2013c0d35b6b340
Andy Tyler <tylera07@mc-n358836.mynet>
2020-05-17 21:48:39 +0100

    Front end feature complete - delete feature operational!

--- index.js
+++ index.js
@@ -7,24 +7,21 @@ const url = document.getElementById('entry-text-box')
 async function getList () {
   const response = await fetch('https://mhs6nqd42a.execute-api.eu-west-1.amazonaws.com/Prod/list/', {})
   const json = await response.json()
-  images = await json.catchphrases
+  images = await json.catchphrases.Items
   
   const display = document.getElementById('display')
   display.innerHTML = ''
   
-  // item <- Data model
-  // url
-  // id
-  // ...
   images.forEach((item) => {
-    var deleteButton = document.createElement('span') // Create a <button> element
-    deleteButton.id = "item.id"
+    var deleteButton = document.createElement('button') // Create a <button> element
+    deleteButton.id = item.id
+    deleteButton.onclick = XHRDelete
     deleteButton.classList += "badge badge-danger badge-pill"
     deleteButton.innerText = "Delete" // Insert text
 
     var URLitem = document.createElement('li') // Create a <button> element
     URLitem.classList += "list-group-item d-flex justify-content-between align-items-center"
-    URLitem.innerHTML = item // Insert text
+    URLitem.innerHTML = item.url // Insert text
     
     display.appendChild(URLitem)
     URLitem.appendChild(deleteButton)
@@ -34,12 +31,14 @@ async function getList () {
   })
 }
 
+
 // $('#submit-button').on('click', function(event) {
 //   event.preventDefault(); // To prevent following the link (optional)
 //   XHRPost()
 // });
 
 async function XHRPost () {
+  
   var http = new XMLHttpRequest()
   var url = 'https://mhs6nqd42a.execute-api.eu-west-1.amazonaws.com/Prod/add/'
   const box = document.getElementById('entry-text-box')
@@ -60,11 +59,14 @@ async function XHRPost () {
 }
 
 async function XHRDelete () {
+  console.log('IN THE DELETE FUNC');
+  const key = this.id
+  
   var http = new XMLHttpRequest()
   var url = 'https://mhs6nqd42a.execute-api.eu-west-1.amazonaws.com/Prod/delete/'
 
-  const box = document.getElementById('entry-text-box')
-  var data = JSON.stringify({ url: box.value })
+  // const box = document.getElementById('entry-text-box')
+  var data = JSON.stringify({ id: key })
   http.open('POST', url, true)
 
   // Send the proper header information along with the request
